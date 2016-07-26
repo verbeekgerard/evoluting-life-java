cf77fd4b5782f317dcc39365e67c5fdbe8f89aa5
Erwin Konink <erwinkonink@gmail.com>
2016-07-26 18:01:26 +0200

    tweaked with cost and reward

--- src/entities/Animal.java
+++ src/entities/Animal.java
@@ -18,214 +18,215 @@ import java.util.Map;
 
 public class Animal extends Organism implements Comparable<Animal> {
 	public Position startPosition;
-	
-	public int leftAccelerate = 0;    // Left angle
-	public int leftDecelerate = 0;    // Right angle
+
+	public int leftAccelerate = 0; // Left angle
+	public int leftDecelerate = 0; // Right angle
 	public int rightAccelerate = 0;// Velocity accelerator
 	public int rightDecelerate = 0; // Velocity suppressor
-	
+
 	public double size;
 	public double travelledDistance = 0;
 	public double collided = 0;
 	public double usedEnergy = 0;
 	public Eyes eyes;
-	
+
 	public double initialEnergy;
 	public double linearFriction;
 	public double angularFriction;
 	public double velocityLeft = 0;
 	public double velocityRight = 0;
-	
+
 	public double linearForce;
-	
-    private Brain brain;
+
+	private Brain brain;
 	private Map<String, Double> output;
-    
-    @Override
-    public double getSize() {
-        return Options.sizeOption.get() * (1 + 0.75 * healthN());
-    }
-    
-    public Map<String, Double> getInitialOutput(){
-    	Map<String, Double> initialOutput = new HashMap<>();
-    	initialOutput.put("leftAccelerate", 0d);
-    	initialOutput.put("leftDecelerate", 0d);
-    	initialOutput.put("rightAccelerate", 0d);
-    	initialOutput.put("rightDecelerate", 0d);
-    	return initialOutput;
-    }
+
+	@Override
+	public double getSize() {
+		return Options.sizeOption.get() * (1 + 0.75 * healthN());
+	}
+
+	public Map<String, Double> getInitialOutput() {
+		Map<String, Double> initialOutput = new HashMap<>();
+		initialOutput.put("leftAccelerate", 0d);
+		initialOutput.put("leftDecelerate", 0d);
+		initialOutput.put("rightAccelerate", 0d);
+		initialOutput.put("rightDecelerate", 0d);
+		return initialOutput;
+	}
 
 	public Animal(Genome genome, Position position, World world) {
 		super(genome, position, world);
-		
+
 		this.startPosition = new Position(position);
 		this.size = Options.sizeOption.get();
-        this.initialEnergy = Options.initialEnergyOption.get();
+		this.initialEnergy = Options.initialEnergyOption.get();
 
-        this.angularFriction = Options.angularFrictionOption.get();
-        this.linearFriction = Options.linearFrictionOption.get();
+		this.angularFriction = Options.angularFrictionOption.get();
+		this.linearFriction = Options.linearFrictionOption.get();
 
-        this.eyes = new Eyes(this, genome.sensor, world);
+		this.eyes = new Eyes(this, genome.sensor, world);
 
-        this.linearForce = genome.movement.linearForce;
+		this.linearForce = genome.movement.linearForce;
 
-        this.brain = new Brain(genome.brain);
-        this.output = getInitialOutput();
+		this.brain = new Brain(genome.brain);
+		this.output = getInitialOutput();
 	}
-	
-	public Double rank() { 
-        return this.getHealth();
-    }
-	
+
+	public Double rank() {
+		return this.getHealth();
+	}
+
 	public double healthN() {
-        double health = this.getHealth();
-        return health > 0 ? 1 - 1 / Math.exp(health / 200) : 0;
+		double health = this.getHealth();
+		return health > 0 ? 1 - 1 / Math.exp(health / 200) : 0;
 	}
-	
-	public List<Double> createInput(FoodVector visibleFood, FoodVector visibleAnimal, double wallDistance) {   
-        double fieldOfView = eyes.fieldOfView;
-        double viewDistance = eyes.viewDistance;
 
-        List<Double> inputs = new ArrayList<>();
-        
-        // left
-        inputs.add(visibleAnimal != null ? (fieldOfView / 2 + visibleAnimal.angle) / fieldOfView : 0);
-        // right
-        inputs.add(visibleAnimal != null ? (fieldOfView / 2 - visibleAnimal.angle) / fieldOfView : 0);
-        // distance
-        inputs.add(visibleAnimal != null ? (viewDistance - visibleAnimal.distance) / viewDistance : 0);
+	public List<Double> createInput(FoodVector visibleFood, FoodVector visibleAnimal, double wallDistance) {
+		double fieldOfView = eyes.fieldOfView;
+		double viewDistance = eyes.viewDistance;
 
-        // left
-        inputs.add(visibleFood != null ? (fieldOfView / 2 + visibleFood.angle) / fieldOfView : 0);
-        // right
-        inputs.add(visibleFood != null ? (fieldOfView / 2 - visibleFood.angle) / fieldOfView : 0);
-        // distance
-        inputs.add(visibleFood != null ? (viewDistance - visibleFood.distance) / viewDistance : 0);
+		List<Double> inputs = new ArrayList<>();
 
-        // distance to wall
-        inputs.add((viewDistance - wallDistance) / viewDistance);
+		// left
+		inputs.add(visibleAnimal != null ? (fieldOfView / 2 + visibleAnimal.angle) / fieldOfView : 0);
+		// right
+		inputs.add(visibleAnimal != null ? (fieldOfView / 2 - visibleAnimal.angle) / fieldOfView : 0);
+		// distance
+		inputs.add(visibleAnimal != null ? (viewDistance - visibleAnimal.distance) / viewDistance : 0);
+
+		// left
+		inputs.add(visibleFood != null ? (fieldOfView / 2 + visibleFood.angle) / fieldOfView : 0);
+		// right
+		inputs.add(visibleFood != null ? (fieldOfView / 2 - visibleFood.angle) / fieldOfView : 0);
+		// distance
+		inputs.add(visibleFood != null ? (viewDistance - visibleFood.distance) / viewDistance : 0);
+
+		// distance to wall
+		inputs.add((viewDistance - wallDistance) / viewDistance);
 		// random
-        inputs.add(new Range(0, 1).random());
+		inputs.add(new Range(0, 1).random());
 
-        double normalizationFactor = (Options.maxThreshold.get() + Options.minThreshold.get()) / 2;
-        // Normalize inputs
-        for (int i = 0; i < inputs.size(); i++) {
-        	Double value = inputs.get(i);
-        	value *= normalizationFactor;
-        	inputs.set(i, value);
-        }
+		double normalizationFactor = (Options.maxThreshold.get() + Options.minThreshold.get()) / 2;
+		// Normalize inputs
+		for (int i = 0; i < inputs.size(); i++) {
+			Double value = inputs.get(i);
+			value *= normalizationFactor;
+			inputs.set(i, value);
+		}
 
-        return inputs;
-    }
-	
+		return inputs;
+	}
+
 	public void checkColission(Organism organism) {
-        if (organism == this) return;
+		if (organism == this)
+			return;
 
-        Position p = this.position;
+		Position p = this.position;
 
-        // Use formula for a circle to find food
-        double x2 = (p.x - organism.position.x); x2 *= x2;
-        double y2 = (p.y - organism.position.y); y2 *= y2;
-        double s2 = organism.getSize() + 2; s2 *= s2;
+		// Use formula for a circle to find food
+		double x2 = (p.x - organism.position.x); x2 *= x2;
+		double y2 = (p.y - organism.position.y); y2 *= y2;
+		double s2 = organism.getSize() + 2; s2 *= s2;
 
-        // Only if we are within the circle, collide it
-        if (x2 + y2 >= s2) {
-          return;
-        }
-        
-        double dx = (Math.cos(p.a) * this.getVelocity());
-        double dy = (Math.sin(p.a) * this.getVelocity());
+		// Only if we are within the circle, collide it
+		if (x2 + y2 >= s2) {
+			return;
+		}
 
-        // Move the entity
-        p.x -= dx;
-        p.y -= dy;
-        
-        // Increase entities total collision counter
-        this.collided += CostCalculator.collide(this.getVelocity());
+		double dx = (Math.cos(p.a) * this.getVelocity());
+		double dy = (Math.sin(p.a) * this.getVelocity());
 
-        // Increment global collision counter
-        Main.getInstance().broadcast(EventType.COLLIDE, collided);
-    }
-	
-	public void collide(List<Plant>plants, List<Animal> animals) {
+		// Move the entity
+		p.x -= dx;
+		p.y -= dy;
+
+		// Increase entities total collision counter
+		this.collided += CostCalculator.collide(this.getVelocity());
+
+		// Increment global collision counter
+		Main.getInstance().broadcast(EventType.COLLIDE, collided);
+	}
+
+	public void collide(List<Plant> plants, List<Animal> animals) {
 		// TODO: optimize this by combining it with the sense action
-		for (int i=0; i<plants.size(); i ++) {
-        	checkColission(plants.get(i));
-        } 
-		for (int i=0; i<animals.size(); i ++) {
-        	checkColission(animals.get(i));
-        } 
-    }
-	
+		for (int i = 0; i < plants.size(); i++) {
+			checkColission(plants.get(i));
+		}
+		for (int i = 0; i < animals.size(); i++) {
+			checkColission(animals.get(i));
+		}
+	}
+
 	public double getVelocity() {
-		return (this.velocityRight + this.velocityLeft)/2;
+		return (this.velocityRight + this.velocityLeft) / 2;
 	}
-	
+
 	public void move() {
-        Position p = this.position;
+		Position p = this.position;
 
-        // Keep angles within bounds
-        p.a = p.a % (Math.PI * 2);
-        if (p.a < 0) p.a += Math.PI * 2;
+		// Keep angles within bounds
+		p.a = p.a % (Math.PI * 2);
+		if (p.a < 0)
+			p.a += Math.PI * 2;
 
-        // F=m*a => a=F/m, dv=a*dt => dv=dt*F/m, dt=one cycle, m=1
-        double accelerationLeft = (this.output.get("leftAccelerate") - this.output.get("leftDecelerate")) * this.linearForce;
-        this.velocityLeft += accelerationLeft;
-        this.velocityLeft -= this.velocityLeft * Options.linearFrictionOption.get();
+		// F=m*a => a=F/m, dv=a*dt => dv=dt*F/m, dt=one cycle, m=1
+		double accelerationLeft = (this.output.get("leftAccelerate") - this.output.get("leftDecelerate"))
+				* this.linearForce;
+		this.velocityLeft += accelerationLeft;
+		this.velocityLeft -= this.velocityLeft * Options.linearFrictionOption.get();
 
-        double accelerationRight = (this.output.get("rightAccelerate") - this.output.get("rightDecelerate")) * this.linearForce;
-        this.velocityRight += accelerationRight;
-        this.velocityRight -= this.velocityRight * Options.linearFrictionOption.get();
+		double accelerationRight = (this.output.get("rightAccelerate") - this.output.get("rightDecelerate"))
+				* this.linearForce;
+		this.velocityRight += accelerationRight;
+		this.velocityRight -= this.velocityRight * Options.linearFrictionOption.get();
 
-        p.a += (this.velocityLeft - this.velocityRight)/10;
+		p.a += (this.velocityLeft - this.velocityRight) / 10;
 
-        // Convert movement vector into polar
-        double dx = (Math.cos(p.a) * this.getVelocity());
-        double dy = (Math.sin(p.a) * this.getVelocity());
+		// Convert movement vector into polar
+		double dx = (Math.cos(p.a) * this.getVelocity());
+		double dy = (Math.sin(p.a) * this.getVelocity());
 
-        // Move the entity
-        p.x += dx;
-        p.y += dy;
+		// Move the entity
+		p.x += dx;
+		p.y += dy;
 
-        // Reward for traveled distance. Maybe redefine the traveled distance.
-        this.travelledDistance = CostCalculator.travelledDistance(this.position.calculateDistance(this.startPosition));
-        
-        // Register the cost of the forces applied for acceleration
-        this.usedEnergy += CostCalculator.rotate(p.a);
-        this.usedEnergy += CostCalculator.accelerate((accelerationLeft + accelerationRight));
-    }
-      
-    @Override
-	public double getHealth() {
-		return this.initialEnergy +
-				this.travelledDistance -
-				this.collided - 
-				this.usedEnergy;
+		// Reward for traveled distance. Maybe redefine the traveled distance.
+		this.travelledDistance = CostCalculator.travelledDistance(this.position.calculateDistance(this.startPosition));
+
+		// Register the cost of the forces applied for acceleration
+		this.usedEnergy += CostCalculator.rotate(p.a);
+		this.usedEnergy += CostCalculator.accelerate((accelerationLeft + accelerationRight));
 	}
-    
-	public void run(List<Plant>plants, List<Animal> animals){
+
+	@Override
+	public double getHealth() {
+		return this.initialEnergy + this.travelledDistance - this.collided - this.usedEnergy;
+	}
+
+	public void run(List<Plant> plants, List<Animal> animals) {
 		this.age++;
 
 		Targets targets = this.eyes.sense(plants, animals);
-        think(targets);
-        move();        
-        collide(plants, animals);
+		think(targets);
+		move();
+		collide(plants, animals);
 
-        // Register the cost of the cycle
-        this.usedEnergy += CostCalculator.cycle();
+		// Register the cost of the cycle
+		this.usedEnergy += CostCalculator.cycle();
 	}
-	
+
 	public void think(Targets targets) {
 		FoodVector plantFoodVector = null;
 		FoodVector animalFoodVector = null;
-		
+
 		if (targets.plants.size() > 0) {
 			plantFoodVector = targets.plants.get(0);
 		}
 		if (targets.animals.size() > 0) {
 			animalFoodVector = targets.animals.get(0);
 		}
-		
+
 		List<Double> inputs = createInput(plantFoodVector, animalFoodVector, targets.wallDistance);
 
 		List<String> keys = new ArrayList<>();
@@ -233,11 +234,11 @@ public class Animal extends Organism implements Comparable<Animal> {
 		keys.add("leftDecelerate");
 		keys.add("rightAccelerate");
 		keys.add("rightDecelerate");
-		
-        List<Double> thoughtOutput = this.brain.think(inputs);
-        for (int i=0; i<thoughtOutput.size();i++) {
-        	this.output.put(keys.get(i), thoughtOutput.get(i));
-        }
+
+		List<Double> thoughtOutput = this.brain.think(inputs);
+		for (int i = 0; i < thoughtOutput.size(); i++) {
+			this.output.put(keys.get(i), thoughtOutput.get(i));
+		}
 	}
 
 	@Override
--- src/main/Population.java
+++ src/main/Population.java
@@ -13,115 +13,112 @@ import evolution.RouletteWheelSelectionByRank;
 import genetics.Genome;
 
 public class Population {
-	
-    public World world;
+
+	public World world;
 	public List<Animal> entities = new CopyOnWriteArrayList<>(); // Slow lost but no exceptions in UI
 	public Animal winningEntity;
-	
-	public Option populationSize = new Option(16*8);
-	
-	public Population(World world){
+
+	public Option populationSize = new Option(16 * 8);
+
+	public Population(World world) {
 		this.world = world;
-		
+
 		// Fill our population with entities
-      for (int i = 0; i < populationSize.get(); i++) {
-          Genome genome = createGenome();
-          Position position = createRandomPosition();
-          Animal entity = new Animal(genome, position, world);
-          // this.entityCreated.notifyAsync(newAnimal);
-          this.entities.add(entity);
-      }
+		for (int i = 0; i < populationSize.get(); i++) {
+			Genome genome = createGenome();
+			Position position = createRandomPosition();
+			Animal entity = new Animal(genome, position, world);
+			// this.entityCreated.notifyAsync(newAnimal);
+			this.entities.add(entity);
+		}
 	}
-    
-    public Genome createGenome() {
-        return new Genome(8, 4);
-    }
-    
-    public Position createRandomPosition() {
-    	return new Position(world.width * Math.random(), world.height * Math.random(), Math.random() * Math.PI * 2);
-    }
-    
-//    getState: function () {
-//          sortSuccess.call(this);
-//          var genomeStates = [];
-//
-//          for (var i = 0; i < this.entities.length; i++) {
-//              var genome = this.entities[i].getGenome();
-//              genomeStates.push(genome.getState());
-//          }
-//
-//          return {genomes: genomeStates};
-//      },
 
-//      loadState: function (genomeStates) {
-//          this.entities = [];
-//          var genomes = genomeStates.genomes;
-//
-//          for (var i = 0; i < genomes.length; i++) {
-//              var genome = new Genome(undefined, undefined, genomes[i]);
-//              var position = createRandomPosition();
-//              var entity = new Animal(genome, position);
-//              entity.population = this;
-//              this.entities.push( entity );
-//          }
-//      },
-
-      public void run(List<Plant> plants) {
-
-    	  Collections.sort(this.entities);
-          // Find the best ranking entity
-          winningEntity = this.entities.get(0);
-
-    	  List<Animal> entitiesToRemove = new ArrayList<>();
-          for (int i = 0; i < this.entities.size(); i++) {
-        	  Animal entity = this.entities.get(i);
-        	  // TODO to make this fast this should run on separate threads
-              entity.run(plants, this.entities);
-
-              // Check entity life cycle and remove dead entities
-              if (!entity.lives()) {
-            	  entitiesToRemove.add(entity);
-              }
-          }
-          
-          for (int i=0; i< entitiesToRemove.size()/2; i++) {
-        	  List<Animal> parents = selectParents();
-              produceChildren(parents);
-          }   
-
-          for (Animal entityToRemove : entitiesToRemove){
-        	  this.entities.remove(entityToRemove);
-          }
-
-          while (this.entities.size() <= populationSize.get() -2) {
-        	  List<Animal> parents = selectParents();
-              produceChildren(parents);
-          }   
-      }
-      
-    public void produceChildren(List<Animal> parents) {
-	    List<Genome> children = parents.get(0).genome.mate(parents.get(1).genome);
-	    for (Genome child : children) {
-	        child.mutate();
-	
-	        // Spawn a new entity from it
-	        Position position = createRandomPosition();
-	        Animal newAnimal = new Animal( child, position, world );
-	        this.entities.add(newAnimal);
-	        Main.getInstance().broadcast(EventType.NEW_ANIMAL, newAnimal);
-	    }
+	public Genome createGenome() {
+		return new Genome(8, 4);
 	}
-      
-    public List<Animal> selectParents() {
-	    List<Animal> parents = new ArrayList<>();
-	    
-	    for (int i=0; i<2; i++) {
-	        Animal winningEntity = new RouletteWheelSelectionByRank().select(this.entities);
-	        parents.add(winningEntity);
-	    }
-	
-	    return parents;
-    }
-    
-    
+
+	public Position createRandomPosition() {
+		return new Position(world.width * Math.random(), world.height * Math.random(), Math.random() * Math.PI * 2);
+	}
+
+	// getState: function () {
+	// sortSuccess.call(this);
+	// var genomeStates = [];
+	//
+	// for (var i = 0; i < this.entities.length; i++) {
+	// var genome = this.entities[i].getGenome();
+	// genomeStates.push(genome.getState());
+	// }
+	//
+	// return {genomes: genomeStates};
+	// },
+
+	// loadState: function (genomeStates) {
+	// this.entities = [];
+	// var genomes = genomeStates.genomes;
+	//
+	// for (var i = 0; i < genomes.length; i++) {
+	// var genome = new Genome(undefined, undefined, genomes[i]);
+	// var position = createRandomPosition();
+	// var entity = new Animal(genome, position);
+	// entity.population = this;
+	// this.entities.push( entity );
+	// }
+	// },
+
+	public void run(List<Plant> plants) {
+		Collections.sort(this.entities);
+		// Find the best ranking entity
+		winningEntity = this.entities.get(0);
+
+		List<Animal> entitiesToRemove = new ArrayList<>();
+		for (int i = 0; i < this.entities.size(); i++) {
+			Animal entity = this.entities.get(i);
+			// TODO to make this fast this should run on separate threads
+			entity.run(plants, this.entities);
+
+			// Check entity life cycle and remove dead entities
+			if (!entity.lives()) {
+				entitiesToRemove.add(entity);
+			}
+		}
+
+		for (int i = 0; i < entitiesToRemove.size() / 2; i++) {
+			List<Animal> parents = selectParents();
+			produceChildren(parents);
+		}
+
+		for (Animal entityToRemove : entitiesToRemove) {
+			this.entities.remove(entityToRemove);
+		}
+
+		while (this.entities.size() <= populationSize.get() - 2) {
+			List<Animal> parents = selectParents();
+			produceChildren(parents);
+		}
+	}
+
+	public void produceChildren(List<Animal> parents) {
+		List<Genome> children = parents.get(0).genome.mate(parents.get(1).genome);
+		for (Genome child : children) {
+			child.mutate();
+
+			// Spawn a new entity from it
+			Position position = createRandomPosition();
+			Animal newAnimal = new Animal(child, position, world);
+			this.entities.add(newAnimal);
+			Main.getInstance().broadcast(EventType.NEW_ANIMAL, newAnimal);
+		}
+	}
+
+	public List<Animal> selectParents() {
+		List<Animal> parents = new ArrayList<>();
+
+		for (int i = 0; i < 2; i++) {
+			Animal winningEntity = new RouletteWheelSelectionByRank().select(this.entities);
+			parents.add(winningEntity);
+		}
+
+		return parents;
+	}
 }
