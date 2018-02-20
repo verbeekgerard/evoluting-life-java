8ef3e2dd45bdab961ec056454eb298fd0f7cf9e2
Erwin Konink <erwinkonink@gmail.com>
2018-02-20 12:02:50 +0100

    experiment with values for extra layer

--- src/main/java/eu/luminis/genetics/LayerGeneBuilder.java
+++ src/main/java/eu/luminis/genetics/LayerGeneBuilder.java
@@ -1,9 +1,12 @@
 package eu.luminis.genetics;
 
+import eu.luminis.Options;
+import eu.luminis.util.Range;
 import org.apache.commons.math3.linear.*;
 
 class LayerGeneBuilder {
     private int size;
+    private double multiplier = new Range(-1 * Options.maxWeight.get(), Options.maxWeight.get()).random();
 
     private LayerGeneBuilder() {
     }
@@ -17,8 +20,13 @@ class LayerGeneBuilder {
         return this;
     }
 
+    public LayerGeneBuilder withMultiplier(double multiplier) {
+        this.multiplier = multiplier;
+        return this;
+    }
+
     public LayerGene build() {
-        RealMatrix matrix = MatrixUtils.createRealIdentityMatrix(size);
+        RealMatrix matrix = MatrixUtils.createRealIdentityMatrix(size).scalarMultiply(multiplier);
         RealVector biases = new ArrayRealVector(size);
         
         return new LayerGene(matrix.getData(), biases.toArray());
